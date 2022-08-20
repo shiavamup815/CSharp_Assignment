7582a2958d9fff28f3058415f4f24c8b5e094419
Shivam Gupta <53276709+shiavamup815@users.noreply.github.com>
2022-08-20 20:03:53 +0530

    Add files via upload

---
+++ Assignment_On_Collection/Non_Generic/Generic_Collection/Generic_Collection/Arraylist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collection
+{
+    internal class Arraylist
+    {
+        public static void Solution()
+        {
+            ArrayList personList = new ArrayList();
+            personList.Add("Sandeep");
+            personList.Add("Raviendra");
+            personList.Add("Aarav");
+            personList.Add("Bholu");
+            personList.Add("Manisha");
+
+
+            foreach (var item in personList)
+            {
+                string arrayItem = string.Format($"Name  is {item}");
+                Console.WriteLine(arrayItem);
+            }
+            personList.Sort();
+            Console.WriteLine(personList);
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment_On_Collection/Non_Generic/Generic_Collection/Generic_Collection/Sorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Collection
+{
+    internal class Sorting
+    {
+        public static void solution()
+        {
+            List<int> ItemList = new List<int>();
+            Console.WriteLine("Enter a number you wanted to store: "); // Outputs List in reverse order. (Recent input first).
+            for (int i = 0; i < 10; i++) // Continue For Loop until i is < the needed amount.
+            {
+                Console.Write($"{i + 1} Number: "); // Asks for user input into array.
+                int ListInput = Convert.ToInt32(Console.ReadLine()); // User inputs value into field.
+                ItemList.Add(ListInput);
+                
+
+            }
+            Console.WriteLine("\n");
+            ItemList.Sort();
+            Console.WriteLine("Numbers in Ascending Order: \n");
+            Console.WriteLine(string.Join(",", ItemList));
+            Console.ReadLine();
+        }
+    }
+}
